1663294c276ba7417938ee82f2879014fc4ab095
Ivan Wilhelm <contato@ivanwilhelm.dev>
2025-05-27 15:34:00 -0300

    refactor: Rename main application class and add configuration files.

--- src/main/kotlin/dev/ivanwilhelm/library/Application.kt
+++ src/main/kotlin/dev/ivanwilhelm/library/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LibraryServiceApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<LibraryServiceApplication>(*args)
+    runApplication<Application>(*args)
 }
